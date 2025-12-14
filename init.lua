691b80389324f828cd9e226ca35dbbe430399ede
greenerino <greenejustint@gmail.com>
2025-12-14 09:45:18 -0800

    Bind vsplit

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-\\>', ':vsplit<CR>')
 
 -- If we're running outside of Nix, then we need to handle plugin installation.
 -- Not technically the right way to do this, since we may want to stub nixCats for non-nix usage and use the plugin's functionality.
