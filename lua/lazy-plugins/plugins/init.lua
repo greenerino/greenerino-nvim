ca644e315daa7598ee82a63bdb901fab62d5e17f
Justin Greene <greenejustint@gmail.com>
2025-12-13 23:22:00 -0800

    Pin nvim-tree to 1.14.0

--- lua/lazy-plugins/plugins/init.lua
+++ lua/lazy-plugins/plugins/init.lua
@@ -1,7 +1,9 @@
+-- Non-nix list of plugins.
+-- Should match what is in NixCats
 return {
     {
         "nvim-tree/nvim-tree.lua",
-        version = "*",
+        version = "1.14.0",
         lazy = false,
         dependencies = {
             "nvim-tree/nvim-web-devicons",
