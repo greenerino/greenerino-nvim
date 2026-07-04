54b3c22f15c342ccac54b881e32be8317d986c6f
greenerino <greenejustint@gmail.com>
2026-07-04 13:16:20 -0700

    Fix some Nixd auto completions and definitions for home manager

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -2,4 +2,17 @@ return {
   cmd = { 'nixd' },
   filetypes = { 'nix' },
   root_markers = { 'flake.nix', '.git' },
+  settings = {
+    nixd = {
+      options = {
+        nixos = {
+          expr = '(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.EncoreNix.options'
+        },
+        home_manager = {
+          expr =
+          '(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.EncoreNix.options.home-manager.users.type.getSubOptions []'
+        },
+      },
+    },
+  },
 }
